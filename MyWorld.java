7d661e0176250419bfd4e55682392fa83974c08e
MandatoryUpdate <118033042+MandatoryUpdate@users.noreply.github.com>
2024-04-19 14:28:49 -0500

    Add files via upload

---
+++ MyWorld.java
@@ -0,0 +1,16 @@
+import mayflower.*;
+
+public class MyWorld extends World
+{
+    public MyWorld()
+    {
+        Mayflower.showBounds(true);
+        buildWorld();
+    }
+    public void act(){
+        
+    }
+    public void buildWorld(){
+        
+    }
+}
