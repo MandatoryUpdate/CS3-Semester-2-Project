5bbc6b4efdd37674ba18a213dbff5b4c0e143257
brandonwchen2 <167358534+brandonwchen2@users.noreply.github.com>
2024-04-19 15:13:28 -0500

    Add files via upload

---
+++ calculate.java
@@ -0,0 +1,11 @@
+import mayflower.*;
+public class calculate
+{
+    public calculate()
+    {
+         
+    }
+    public double calculate(double multiplier, double active, double upgrades){
+        return (multiplier*active)+upgrades;
+    }
+}
