60246edce42de1e2a3a2eb71ce2698121bb0b9a7
brandonwchen2 <167358534+brandonwchen2@users.noreply.github.com>
2024-04-19 14:39:07 -0500

    Add files via upload

---
+++ oneTimeUpgrade.java
@@ -0,0 +1,16 @@
+import mayflower.*;
+public class oneTimeUpgrade extends Actor
+{
+    private boolean bought;
+    private boolean unlocked;
+    private double multiplierVal;
+    
+    public oneTimeUpgrade(){
+        boolean bought = false;
+        boolean unlocked = false;
+        double multiplierVal = 1;
+    }
+    public void act(){
+        
+    }
+}
