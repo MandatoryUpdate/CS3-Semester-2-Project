0ac939e13095d1f60de1172e177ae5cc2fa29fae
MandatoryUpdate <118033042+MandatoryUpdate@users.noreply.github.com>
2024-04-19 14:28:44 -0500

    Add files via upload

---
+++ MyMayflower.java
@@ -0,0 +1,17 @@
+import mayflower.*;
+
+public class MyMayflower extends Mayflower
+{
+    //Constructor
+    public MyMayflower()
+    {
+        //Create a window with 800x600 resolution
+        super("Lab 6", 800, 600);
+    }
+
+    public void init()
+    {
+        //Change this to true to run this program in fullscreen mode
+        Mayflower.setFullScreen(false);
+    }
+}
