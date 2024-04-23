ad00deb7bc6339dc023caa2c9926abb35f23ad4b
MandatoryUpdate <118033042+MandatoryUpdate@users.noreply.github.com>
2024-04-23 14:36:39 -0500

    Update MyWorld.java

--- MyWorld.java
+++ MyWorld.java
@@ -2,15 +2,16 @@ import mayflower.*;
 
 public class MyWorld extends World
 {
+    private Rice rice;
     public MyWorld()
     {
         Mayflower.showBounds(true);
-        buildWorld();
+        setBackground("Curry Pictures/Bluebonnet-8100.jpg");
+        rice = new Rice();
+        addObject(rice, 400, 100);
     }
     public void act(){
-        
+        showText("" + rice.getRiceCounter(), 100, 100);
     }
-    public void buildWorld(){
-        
-    }
+    
 }
