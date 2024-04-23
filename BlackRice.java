c96bf1b2dd29ab597ac58e04912633db3a78e114
dhanushmitt <167135731+dhanushmitt@users.noreply.github.com>
2024-04-23 15:16:38 -0500

    Update BlackRice.java

--- BlackRice.java
+++ BlackRice.java
@@ -3,6 +3,7 @@ public class BlackRice extends Upgrades{
 
     public BlackRice(){
         super(0,200,1000);
+        setImage(new MayflowerImage("img/BlackRice.png"));
     }
     
     public void act(){
