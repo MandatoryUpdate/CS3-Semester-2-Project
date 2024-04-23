073e149f52db6120c08876f4fd94f29a52bf3b23
dhanushmitt <167135731+dhanushmitt@users.noreply.github.com>
2024-04-23 15:18:12 -0500

    Update WhiteRice.java

--- WhiteRice.java
+++ WhiteRice.java
@@ -2,6 +2,7 @@ import mayflower.*;
 public class WhiteRice extends Upgrades{
     public WhiteRice(){
         super(0,5,20);
+        setImage(new MayflowerImage("img/WhiteRice.png"));
     }
     
     public void act(){
