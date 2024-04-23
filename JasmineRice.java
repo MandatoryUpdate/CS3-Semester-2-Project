e5dc040f8cbd6ff2d271ac36ccd8d122a73cf919
dhanushmitt <167135731+dhanushmitt@users.noreply.github.com>
2024-04-23 15:17:55 -0500

    Update JasmineRice.java

--- JasmineRice.java
+++ JasmineRice.java
@@ -2,6 +2,7 @@ import mayflower.*;
 public class JasmineRice extends Upgrades{
     public JasmineRice(){
         super(0,20,100);
+        setImage(new MayflowerImage("img/JasmineRice.png"));
     }
     
     public void act(){
