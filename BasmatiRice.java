a872161cfc51cd38f2432a79a3edd1d58fa0fe28
dhanushmitt <167135731+dhanushmitt@users.noreply.github.com>
2024-04-23 15:15:50 -0500

    Update BasmatiRice.java

--- BasmatiRice.java
+++ BasmatiRice.java
@@ -3,6 +3,7 @@ public class BasmatiRice extends Upgrades{
 
     public BasmatiRice(){
         super(0,5000,50000);
+        setImage(new MayflowerImage("img/BasmatiRice.png"));
     }
     
     public void act(){
