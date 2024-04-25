db843441eabda7e14c57458d77e8b9ca029ba394
brandonwchen2 <167358534+brandonwchen2@users.noreply.github.com>
2024-04-25 14:52:57 -0500

    Update MyWorld.java

--- MyWorld.java
+++ MyWorld.java
@@ -4,15 +4,28 @@ public class MyWorld extends World
 {
     private Rice rice;
     private chives chive;
-    private double total;
+    private eggs egg;
+    private curry curry;
+    private paneer paneer;
+    private soysauce soysauce;
+    private tofu tofu;
     public MyWorld()
     {
         Mayflower.showBounds(true);
-        setBackground("img/Curry.png");
         rice = new Rice();
+        egg = new eggs();
         chive = new chives();
+        curry = new curry();
+        paneer = new paneer();
+        soysauce = new soysauce();
+        tofu = new tofu();
         addObject(rice, 400, 400);
         addObject(chive,300,300);
+        addObject(egg,300,200);
+        addObject(curry,300,100);
+        addObject(paneer,100,400);
+        addObject(tofu,500,500);
+        addObject(soysauce,100,100);
     }
     public void act(){
         showText("" + rice.getRiceCounter(), 100, 100); 
