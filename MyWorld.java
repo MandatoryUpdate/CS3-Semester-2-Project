6df7c046cd1696b2165d5451bea0d7f08a471aa5
MandatoryUpdate <118033042+MandatoryUpdate@users.noreply.github.com>
2024-04-25 14:34:24 -0500

    Update MyWorld.java

--- MyWorld.java
+++ MyWorld.java
@@ -4,6 +4,7 @@ public class MyWorld extends World
 {
     private Rice rice;
     private chives chive;
+    private double total;
     public MyWorld()
     {
         Mayflower.showBounds(true);
@@ -14,7 +15,10 @@ public class MyWorld extends World
         addObject(chive,300,300);
     }
     public void act(){
-        showText("" + rice.getRiceCounter(), 100, 100);
+        showText("" + rice.getRiceCounter(), 100, 100); 
+    }
+    public void calculate(double multiplier, double active, double upgrades){
+        total = (multiplier*active)+upgrades;
     }
     
 }
