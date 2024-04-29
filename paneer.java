676b0d2fbf422b8c8030a707c10f8bfb10209360
MandatoryUpdate <118033042+MandatoryUpdate@users.noreply.github.com>
2024-04-29 15:31:56 -0500

    Update paneer.java

--- paneer.java
+++ paneer.java
@@ -4,24 +4,29 @@ public class paneer extends oneTimeUpgrade
 {
     private boolean bought;
     private int cost;
+    private double multiplier;
     public paneer()
     {
         MayflowerImage p = new MayflowerImage ("img/Paneer.png");
-        p.scale(80,80);
+        p.scale(200,200);
         setImage(p);
-        cost = 40;
+        cost = 100000;
+        multiplier = 1.5;
     }
     public int getCost()
     {
         return cost;
     }
+    public double getMultiplier()
+    {
+        return multiplier;
+    }
     public void act()
     {
         if(Mayflower.mouseClicked(this)){
             MayflowerImage p2 = new MayflowerImage ("img/Paneer2.png");
-            p2.scale(80,80);
+            p2.scale(200,200);
             setImage(p2);
-            super.addMultiplier(2);
         }
     }
 }
