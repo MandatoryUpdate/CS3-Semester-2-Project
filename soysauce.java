c5d00721f51ae7ec87135aeffe572c8bce628d33
MandatoryUpdate <118033042+MandatoryUpdate@users.noreply.github.com>
2024-04-29 15:32:36 -0500

    Update soysauce.java

--- soysauce.java
+++ soysauce.java
@@ -4,24 +4,29 @@ public class soysauce extends oneTimeUpgrade
 {
     private boolean bought;
     private int cost;
+    private double multiplier;
     public soysauce()
     {
         MayflowerImage p = new MayflowerImage ("img/SoySauce.png");
-        p.scale(80,90);
+        p.scale(200,200);
         setImage(p);
-        cost = 50;
+        cost = 5000;
+        multiplier = 1.5;
     }
     public int getCost()
     {
         return cost;
     }
+    public double getMultiplier()
+    {
+        return multiplier;
+    }
     public void act()
     {
         if(Mayflower.mouseClicked(this)){
             MayflowerImage p2 = new MayflowerImage ("img/SoySauce2.png");
-            p2.scale(80,90);
+            p2.scale(200,200);
             setImage(p2);
-            super.addMultiplier(2);
         }
     }
 }
