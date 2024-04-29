42a45e9a7f317edeba2f2479ec1097d6dd88f6b2
MandatoryUpdate <118033042+MandatoryUpdate@users.noreply.github.com>
2024-04-29 15:32:16 -0500

    Update chives.java

--- chives.java
+++ chives.java
@@ -4,24 +4,29 @@ public class chives extends oneTimeUpgrade
 {
     private boolean bought;
     private int cost;
+    private double multiplier;
     public chives()
     {
         MayflowerImage p = new MayflowerImage ("img/Chives.png");
-        p.scale(80,80);
+        p.scale(200,200);
         setImage(p);
-        cost = 20;
+        cost = 110;
+        multiplier = 1.5;
     }
     public int getCost()
     {
         return cost;
     }
+    public double getMultiplier()
+    {
+        return multiplier;
+    }
     public void act()
     {
         if(Mayflower.mouseClicked(this)){
             MayflowerImage p2 = new MayflowerImage ("img/Chives2.png");
-            p2.scale(80,80);
+            p2.scale(200,200);
             setImage(p2);
-            super.addMultiplier(2);
         }
     }
 }
