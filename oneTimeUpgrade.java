e1db22e5075709e05b91d853d63469209cb02f67
MandatoryUpdate <118033042+MandatoryUpdate@users.noreply.github.com>
2024-04-29 10:02:11 -0500

    Update oneTimeUpgrade.java

--- oneTimeUpgrade.java
+++ oneTimeUpgrade.java
@@ -3,12 +3,17 @@ public class oneTimeUpgrade extends Actor
 {
     private boolean bought;
     private boolean unlocked;
+    private int cost;
     private double multiplierVal;
     
     public oneTimeUpgrade(){
         boolean bought = false;
         boolean unlocked = false;
         double multiplierVal = 1;
+    }
+    public int getCost()
+    {
+        return cost;
     }
     public void act(){
         
