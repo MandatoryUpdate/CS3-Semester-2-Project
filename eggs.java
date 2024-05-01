a2630e22cd200de490e5e934d58280777de0cedb
MandatoryUpdate <118033042+MandatoryUpdate@users.noreply.github.com>
2024-05-01 11:47:18 -0500

    Update eggs.java

--- eggs.java
+++ eggs.java
@@ -10,16 +10,19 @@ public class eggs extends oneTimeUpgrade
         MayflowerImage p = new MayflowerImage ("img/Egg.png");
         p.scale(200,200);
         setImage(p);
-        cost = 100;
+        cost = 1;
         multiplier = 1.5;
     }
     public int getCost()
     {
         return cost;
     }
-    public double getMultiplier()
-    {
+    public double getMultiplier(){
         return multiplier;
+    }
+    public boolean getBought()
+    {
+        return bought;        
     }
     public void act()
     {
@@ -27,6 +30,7 @@ public class eggs extends oneTimeUpgrade
             MayflowerImage p2 = new MayflowerImage ("img/Egg2.png");
             p2.scale(200,200);
             setImage(p2);
+            bought = true;
         }
     }
 }
