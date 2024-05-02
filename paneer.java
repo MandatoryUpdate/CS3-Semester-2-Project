f34f6f0aeae83e7106d905d43c1bec015fcba593
brandonwchen2 <167358534+brandonwchen2@users.noreply.github.com>
2024-05-01 20:51:37 -0500

    Update paneer.java

--- paneer.java
+++ paneer.java
@@ -8,7 +8,7 @@ public class paneer extends oneTimeUpgrade
     public paneer()
     {
         MayflowerImage p = new MayflowerImage ("img/Paneer.png");
-        p.scale(200,200);
+        p.scale(80,80);
         setImage(p);
         cost = 1;
         multiplier = 1.5;
@@ -28,7 +28,7 @@ public class paneer extends oneTimeUpgrade
     {
         if(Mayflower.mouseClicked(this)){
             MayflowerImage p2 = new MayflowerImage ("img/Paneer2.png");
-            p2.scale(200,200);
+            p2.scale(80,80);
             setImage(p2);
             bought = true;
         }
