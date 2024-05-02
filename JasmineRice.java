937a1a497c783fe39f56b485768912cedcc0cf88
brandonwchen2 <167358534+brandonwchen2@users.noreply.github.com>
2024-05-01 21:12:04 -0500

    Update JasmineRice.java

--- JasmineRice.java
+++ JasmineRice.java
@@ -1,12 +1,21 @@
 import mayflower.*;
 public class JasmineRice extends Upgrades{
+    private boolean bought;
+    private int cost;
     public JasmineRice(){
         super(0,20,100);
-        setImage(new MayflowerImage("img/JasmineRice.png"));
+        MayflowerImage p1 = new MayflowerImage ("img/JasmineRice.png");
+        p1.scale(150,150);
+        setImage(p1);
     }
     
     public void act(){
-        
+        if(Mayflower.mouseClicked(this)){
+            MayflowerImage p2 = new MayflowerImage ("img/JasmineRice2.png");
+            p2.scale(150,150);
+            setImage(p2);
+            bought = true;
+        }
     }
     
 }
