1a937be348586c54da1bd402bb8acd0b045ca82a
brandonwchen2 <167358534+brandonwchen2@users.noreply.github.com>
2024-05-01 21:11:44 -0500

    Update BrownRice.java

--- BrownRice.java
+++ BrownRice.java
@@ -1,12 +1,21 @@
 import mayflower.*;
 public class BrownRice extends Upgrades{
+    private boolean bought;
+    private int cost;
     public BrownRice(){
         super(0,75,500);
-        setImage(new MayflowerImage("img/BrownRice.png"));
+        MayflowerImage p1 = new MayflowerImage ("img/BrownRice.png");
+        p1.scale(150,100);
+        setImage(p1);
     }
     
     public void act(){
-        
+        if(Mayflower.mouseClicked(this)){
+            MayflowerImage p2 = new MayflowerImage ("img/BrownRice2.png");
+            p2.scale(150,100);
+            setImage(p2);
+            bought = true;
+        }
     }
     
 }
