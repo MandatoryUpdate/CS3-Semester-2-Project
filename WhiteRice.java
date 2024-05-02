3bd9d418b85096402376943bc72c8b3c1f456cf6
brandonwchen2 <167358534+brandonwchen2@users.noreply.github.com>
2024-05-01 21:12:24 -0500

    Update WhiteRice.java

--- WhiteRice.java
+++ WhiteRice.java
@@ -1,12 +1,21 @@
 import mayflower.*;
 public class WhiteRice extends Upgrades{
+    private boolean bought;
+    private int cost;
     public WhiteRice(){
         super(0,5,20);
-        setImage(new MayflowerImage("img/WhiteRice.png"));
+        MayflowerImage p1 = new MayflowerImage ("img/WhiteRice.png");
+        p1.scale(100,100);
+        setImage(p1);
     }
     
     public void act(){
-        
+        if(Mayflower.mouseClicked(this)){
+            MayflowerImage p2 = new MayflowerImage ("img/WhiteRice2.png");
+            p2.scale(100,100);
+            setImage(p2);
+            bought = true;
+        }
     }
     
 }
