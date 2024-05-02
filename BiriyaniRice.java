56efe2373874e8b652a0316aeb32390954e69390
brandonwchen2 <167358534+brandonwchen2@users.noreply.github.com>
2024-05-01 21:11:13 -0500

    Update BiriyaniRice.java

--- BiriyaniRice.java
+++ BiriyaniRice.java
@@ -1,13 +1,21 @@
 import mayflower.*;
 public class BiriyaniRice extends Upgrades{
-
+    private boolean bought;
+    private int cost;
     public BiriyaniRice(){
         super(0,600,5000);
-        setImage(new MayflowerImage("img/BiriyaniRice.png"));
+        MayflowerImage p1 = new MayflowerImage ("img/BiriyaniRice.png");
+        p1.scale(100,100);
+        setImage(p1);
     }
     
     public void act(){
-        
+        if(Mayflower.mouseClicked(this)){
+            MayflowerImage p2 = new MayflowerImage ("img/BiriyaniRice2.png");
+            p2.scale(100,100);
+            setImage(p2);
+            bought = true;
+        }
     }
     
 }
