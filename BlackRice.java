f8004baad3397db82cbc9cbd06674c006a6396af
brandonwchen2 <167358534+brandonwchen2@users.noreply.github.com>
2024-05-01 21:11:31 -0500

    Update BlackRice.java

--- BlackRice.java
+++ BlackRice.java
@@ -1,13 +1,21 @@
 import mayflower.*;
 public class BlackRice extends Upgrades{
-
+    private boolean bought;
+    private int cost;
     public BlackRice(){
         super(0,200,1000);
-        setImage(new MayflowerImage("img/BlackRice.png"));
+        MayflowerImage p1 = new MayflowerImage ("img/BlackRice.png");
+        p1.scale(100,100);
+        setImage(p1);
     }
     
     public void act(){
-        
+        if(Mayflower.mouseClicked(this)){
+            MayflowerImage p2 = new MayflowerImage ("img/BlackRice2.png");
+            p2.scale(100,100);
+            setImage(p2);
+            bought = true;
+        }
     }
     
 }
