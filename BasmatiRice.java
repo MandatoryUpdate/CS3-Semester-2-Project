87be629925fa3772671e6cffae2f4f30764a404f
brandonwchen2 <167358534+brandonwchen2@users.noreply.github.com>
2024-05-01 21:10:55 -0500

    Update BasmatiRice.java

--- BasmatiRice.java
+++ BasmatiRice.java
@@ -1,13 +1,21 @@
 import mayflower.*;
 public class BasmatiRice extends Upgrades{
-
+    private boolean bought;
+    private int cost;
     public BasmatiRice(){
         super(0,5000,50000);
-        setImage(new MayflowerImage("img/BasmatiRice.png"));
+        MayflowerImage p1 = new MayflowerImage ("img/BasmatiRice.png");
+        p1.scale(100,100);
+        setImage(p1);
     }
     
     public void act(){
-        
+        if(Mayflower.mouseClicked(this)){
+            MayflowerImage p2 = new MayflowerImage ("img/BasmatiRice2.png");
+            p2.scale(100,100);
+            setImage(p2);
+            bought = true;
+        }
     }
     
 }
