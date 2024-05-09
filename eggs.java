0a59b51b4db7b7045f6a46278c88e17f36cb1b44
MandatoryUpdate <118033042+MandatoryUpdate@users.noreply.github.com>
2024-05-09 15:31:04 -0500

    Update eggs.java

--- eggs.java
+++ eggs.java
@@ -2,9 +2,6 @@ import mayflower.*;
 
 public class eggs extends oneTimeUpgrade
 {
-    private boolean bought;
-    private int cost;
-    private double multiplier;
     public eggs()
     {
         super(false, 10, 1.5);
