13f8e0594f57ff9a58ab4df3f780b2c39392496c
MandatoryUpdate <118033042+MandatoryUpdate@users.noreply.github.com>
2024-05-09 15:32:02 -0500

    Update curry.java

--- curry.java
+++ curry.java
@@ -2,35 +2,11 @@ import mayflower.*;
 
 public class curry extends oneTimeUpgrade
 {
-    private boolean bought;
-    private int cost;
-    private double multipler;
     public curry()
     {
+        super(false, 10, 1.5);
         MayflowerImage p = new MayflowerImage ("img/Curry.png");
         p.scale(80,80);
         setImage(p);
-        cost = 1;
-        multipler = 1.5;
-    }
-    public int getCost()
-    {
-        return cost;
-    }
-    public double getMultiplier(){
-        return multipler;
-    }
-    public boolean getBought()
-    {
-        return bought;        
-    }
-    public void act()
-    {
-        if(Mayflower.mouseClicked(this)){
-            MayflowerImage p2 = new MayflowerImage ("img/Curry2.png");
-            p2.scale(80,80);
-            setImage(p2);
-            bought = true;
-        }
     }
 }
