5be707e21c5cb13b58c3ff9a121ace5d0d6313c7
MandatoryUpdate <118033042+MandatoryUpdate@users.noreply.github.com>
2024-05-09 15:31:18 -0500

    Update chives.java

--- chives.java
+++ chives.java
@@ -2,9 +2,6 @@ import mayflower.*;
 
 public class chives extends oneTimeUpgrade
 {
-    private boolean bought;
-    private int cost;
-    private double multiplier;
     public chives()
     {
         super(false, 10, 1.5);
