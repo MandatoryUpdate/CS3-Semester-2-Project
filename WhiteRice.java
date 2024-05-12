9e670f49f521387b8f9a114bf52ce3d19f111ce8
MandatoryUpdate <118033042+MandatoryUpdate@users.noreply.github.com>
2024-05-12 14:57:21 -0500

    Update WhiteRice.java

--- WhiteRice.java
+++ WhiteRice.java
@@ -3,7 +3,7 @@ public class WhiteRice extends Upgrades{
     private boolean bought;
     private int cost;
     public WhiteRice(){
-        super(0,5,20);
+        super(0,5,20, 10000);
         MayflowerImage p1 = new MayflowerImage ("img/WhiteRice.png");
         p1.scale(100,100);
         setImage(p1);
