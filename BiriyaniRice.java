f1b269268de4a0499b9dd83bbe29e29dcda403fd
MandatoryUpdate <118033042+MandatoryUpdate@users.noreply.github.com>
2024-05-12 14:56:08 -0500

    Update BiriyaniRice.java

--- BiriyaniRice.java
+++ BiriyaniRice.java
@@ -3,7 +3,7 @@ public class BiriyaniRice extends Upgrades{
     private boolean bought;
     private int cost;
     public BiriyaniRice(){
-        super(0,600,5000);
+        super(0,600,5000, 10000);
         MayflowerImage p1 = new MayflowerImage ("img/BiriyaniRice.png");
         p1.scale(100,100);
         setImage(p1);
