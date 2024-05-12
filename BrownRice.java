97450c9170fd2832309ac0b631709989bafb944e
MandatoryUpdate <118033042+MandatoryUpdate@users.noreply.github.com>
2024-05-12 14:56:47 -0500

    Update BrownRice.java

--- BrownRice.java
+++ BrownRice.java
@@ -3,7 +3,7 @@ public class BrownRice extends Upgrades{
     private boolean bought;
     private int cost;
     public BrownRice(){
-        super(0,75,500);
+        super(0,75,500, 10000);
         MayflowerImage p1 = new MayflowerImage ("img/BrownRice.png");
         p1.scale(150,100);
         setImage(p1);
