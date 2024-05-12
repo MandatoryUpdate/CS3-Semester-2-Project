27fead0775c8c819885801f752eafdba0fa96b40
MandatoryUpdate <118033042+MandatoryUpdate@users.noreply.github.com>
2024-05-12 14:55:37 -0500

    Update BasmatiRice.java

--- BasmatiRice.java
+++ BasmatiRice.java
@@ -3,7 +3,7 @@ public class BasmatiRice extends Upgrades{
     private boolean bought;
     private int cost;
     public BasmatiRice(){
-        super(0,5000,50000);
+        super(0,5000,50000, 10000);
         MayflowerImage p1 = new MayflowerImage ("img/BasmatiRice.png");
         p1.scale(100,100);
         setImage(p1);
