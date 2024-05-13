c32dcc3d457b2e7dbb62b246bc4383fbfc2d0ae3
brandonwchen2 <167358534+brandonwchen2@users.noreply.github.com>
2024-05-13 14:30:16 -0500

    Update oneTimeUpgrade.java

--- oneTimeUpgrade.java
+++ oneTimeUpgrade.java
@@ -4,15 +4,17 @@ public class oneTimeUpgrade extends Actor
     private boolean bought;
     private int cost;
     private double multiplier;
+    private String name;
     public oneTimeUpgrade(){
         bought = false;
         multiplier = 0;
         cost = 0;
     }
-    public oneTimeUpgrade(boolean b, int c, double m){
+    public oneTimeUpgrade(boolean b, int c, double m, String n){
         bought = b;
         multiplier = m;
         cost = c;
+        name = n;
     }
     public int getCost()
     {
@@ -26,8 +28,8 @@ public class oneTimeUpgrade extends Actor
         MyWorld myWorld = (MyWorld)getWorld();
         if(Mayflower.mouseClicked(this) && myWorld.getTotal() > cost){
             myWorld.subtractCost(cost);
-            MayflowerImage p2 = new MayflowerImage ("img/Egg2.png");
-            p2.scale(50,50);
+            MayflowerImage p2 = new MayflowerImage ("img/"+name+"2.png");
+            p2.scale(80,80);
             setImage(p2);
             bought = true;
         }
