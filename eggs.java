cd3f4a9934735df5c771e37176a358cd5b2c964c
MandatoryUpdate <118033042+MandatoryUpdate@users.noreply.github.com>
2024-05-15 14:59:07 -0500

    Update eggs.java

--- eggs.java
+++ eggs.java
@@ -4,7 +4,7 @@ public class eggs extends oneTimeUpgrade
 {
     public eggs()
     {
-        super(false, 10, 1.5);
+        super(false, 10, 1.5, "Egg");
         MayflowerImage p = new MayflowerImage ("img/Egg.png");
         p.scale(50,50);
         setImage(p);
