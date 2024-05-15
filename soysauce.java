316d5adfeddddbdfc201b236afc4a44e4fba8823
MandatoryUpdate <118033042+MandatoryUpdate@users.noreply.github.com>
2024-05-15 14:51:06 -0500

    Update soysauce.java

--- soysauce.java
+++ soysauce.java
@@ -4,7 +4,7 @@ public class soysauce extends oneTimeUpgrade
 {
     public soysauce()
     {
-        super(false, 10, 1.5);
+        super(false, 10, 1.5, "SoySauce");
         MayflowerImage p = new MayflowerImage ("img/SoySauce.png");
         p.scale(80,90);
         setImage(p);
