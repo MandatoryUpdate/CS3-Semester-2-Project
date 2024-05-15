eb602a0b5159de41d76faec81252a4be273a870b
MandatoryUpdate <118033042+MandatoryUpdate@users.noreply.github.com>
2024-05-15 14:57:46 -0500

    Update sellButton.java

--- sellButton.java
+++ sellButton.java
@@ -4,20 +4,19 @@ public class sellButton extends Actor
 {
     public sellButton()
     {
-        
     }
     public void checkClicked(int val)
     {
         if(val == 1)
         {
             MayflowerImage p = new MayflowerImage ("img/sell.png");
-            p.scale(250,250);
+            p.scale(100,100);
             setImage(new MayflowerImage(p));
         }
         else if(val == 2)
         {
             MayflowerImage p2 = new MayflowerImage ("img/sell2.png");
-            p2.scale(250,250);
+            p2.scale(100,100);
             setImage(new MayflowerImage(p2));
         }
     }
