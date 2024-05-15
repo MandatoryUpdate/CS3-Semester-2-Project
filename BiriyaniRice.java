04d45ccf80c031144b615c0b3c844666a0eaa81b
MandatoryUpdate <118033042+MandatoryUpdate@users.noreply.github.com>
2024-05-15 14:50:14 -0500

    Update BiriyaniRice.java

--- BiriyaniRice.java
+++ BiriyaniRice.java
@@ -9,13 +9,4 @@ public class BiriyaniRice extends Upgrades{
         setImage(p1);
     }
     
-    public void act(){
-        if(Mayflower.mouseClicked(this)){
-            MayflowerImage p2 = new MayflowerImage ("img/BiriyaniRice2.png");
-            p2.scale(100,100);
-            setImage(p2);
-            bought = true;
-        }
-    }
-    
 }
