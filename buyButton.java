86b3733fd47332220b57ca360c067d48b01e24ec
MandatoryUpdate <118033042+MandatoryUpdate@users.noreply.github.com>
2024-05-15 14:58:07 -0500

    Update buyButton.java

--- buyButton.java
+++ buyButton.java
@@ -3,20 +3,19 @@ public class buyButton extends Actor
 {
     public buyButton()
     {
-        
     }
     public void checkClicked(int val)
     {
         if(val == 1)
         {
-            MayflowerImage p = new MayflowerImage ("img/buy.png");
-            p.scale(250,250);
+            MayflowerImage p = new MayflowerImage ("img/buy2.png");
+            p.scale(100,100);
             setImage(new MayflowerImage(p));
         }
         else if(val == 2)
         {
-            MayflowerImage p2 = new MayflowerImage ("img/buy2.png");
-            p2.scale(250,250);
+            MayflowerImage p2 = new MayflowerImage ("img/buy.png");
+            p2.scale(100,100);
             setImage(new MayflowerImage(p2));
         }
     }
