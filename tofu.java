095d61f9b3441e38fc9cdd9ff08aef6d91c0972d
MandatoryUpdate <118033042+MandatoryUpdate@users.noreply.github.com>
2024-05-15 14:59:23 -0500

    Update tofu.java

--- tofu.java
+++ tofu.java
@@ -4,7 +4,7 @@ public class tofu extends oneTimeUpgrade
 {
     public tofu()
     {
-        super(false, 10, 1.5);
+        super(false, 10, 1.5, "tofu");
         MayflowerImage p = new MayflowerImage ("img/tofu.png");
         p.scale(80,80);
         setImage(p);
