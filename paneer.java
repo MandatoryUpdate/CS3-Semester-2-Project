b7dc5b178c5cdc088e250e73eb0c69484f89c6b3
brandonwchen2 <167358534+brandonwchen2@users.noreply.github.com>
2024-05-21 09:09:21 -0500

    Update paneer.java

--- paneer.java
+++ paneer.java
@@ -4,7 +4,7 @@ public class paneer extends oneTimeUpgrade
 {
     public paneer()
     {
-        super(false, 10, 1.5, "Paneer");
+        super(false, 500, 50, "Paneer");
         MayflowerImage p = new MayflowerImage ("img/Paneer.png");
         p.scale(80,80);
         setImage(p);
