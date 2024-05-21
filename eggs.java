2eb11ea284869627e5ab89fddaa0444b7aad8abd
brandonwchen2 <167358534+brandonwchen2@users.noreply.github.com>
2024-05-20 23:38:58 -0500

    Update eggs.java

--- eggs.java
+++ eggs.java
@@ -6,7 +6,7 @@ public class eggs extends oneTimeUpgrade
     {
         super(false, 10, 1.5, "Egg");
         MayflowerImage p = new MayflowerImage ("img/Egg.png");
-        p.scale(50,50);
+        p.scale(80,80);
         setImage(p);
     }
 }
