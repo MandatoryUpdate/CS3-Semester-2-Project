1e53d799b64c29bd5c0a084f92483379441168d1
MandatoryUpdate <118033042+MandatoryUpdate@users.noreply.github.com>
2024-05-21 08:41:36 -0500

    Update BlackRice.java

--- BlackRice.java
+++ BlackRice.java
@@ -3,7 +3,7 @@ public class BlackRice extends Upgrades{
     private boolean bought;
     private int cost;
     public BlackRice(){
-        super(0,200,1000, 10000);
+        super(0,200,1000, 10000, 645, 188);
         MayflowerImage p1 = new MayflowerImage ("img/BlackRice.png");
         p1.scale(115,115);
         setImage(p1);
