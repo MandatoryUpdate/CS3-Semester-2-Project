6cc023d2a8a13ce48154b01c290dc8b9942dee1a
brandonwchen2 <167358534+brandonwchen2@users.noreply.github.com>
2024-05-21 09:23:24 -0500

    Update BlackRice.java

--- BlackRice.java
+++ BlackRice.java
@@ -4,7 +4,7 @@ public class BlackRice extends Upgrades{
     private int cost;
     public BlackRice(){
         //The order is as follows, buildings (amount of buildings), production (how much they make), cost, timer (how long it takes to gen), positionX, positionY
-        super(0,5,1000, 10000, 5000,285);
+        super(0,5,1000, 4500, 645,285);
         MayflowerImage p1 = new MayflowerImage ("img/BlackRice.png");
         p1.scale(115,115);
         setImage(p1);
