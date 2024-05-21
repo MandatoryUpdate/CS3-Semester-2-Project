f68bab71b24798f2bae1db45e6412e1ba6193c81
brandonwchen2 <167358534+brandonwchen2@users.noreply.github.com>
2024-05-21 09:13:48 -0500

    Update JasmineRice.java

--- JasmineRice.java
+++ JasmineRice.java
@@ -4,7 +4,7 @@ public class JasmineRice extends Upgrades{
     private int cost;
     public JasmineRice(){
         //The order is as follows, buildings (amount of buildings), production (how much they make), cost, timer (how long it takes to gen), positionX, positionY
-        super(0,2,100, 10000, 645, 188);
+        super(0,2,100, 10000, 5000, 188);
         MayflowerImage p1 = new MayflowerImage ("img/JasmineRice.png");
         p1.scale(115,115);
         setImage(p1);
