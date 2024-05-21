881cabec55a3118179bb6fb48291770973cda8a8
brandonwchen2 <167358534+brandonwchen2@users.noreply.github.com>
2024-05-21 09:05:59 -0500

    Update BrownRice.java

--- BrownRice.java
+++ BrownRice.java
@@ -4,7 +4,7 @@ public class BrownRice extends Upgrades{
     private int cost;
     public BrownRice(){
         //The order is as follows, buildings (amount of buildings), production (how much they make), cost, timer (how long it takes to gen), positionX, positionY
-        super(0,75,500, 10000, 642, 305);
+        super(0,75,20, 10000, 642, 305);
         MayflowerImage p1 = new MayflowerImage ("img/BrownRice.png");
         p1.scale(135,90);
         setImage(p1);
