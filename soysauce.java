436dc0c7c5bb5f2575d5abb64233c0ba3e83326a
brandonwchen2 <167358534+brandonwchen2@users.noreply.github.com>
2024-05-21 09:09:37 -0500

    Update soysauce.java

--- soysauce.java
+++ soysauce.java
@@ -4,7 +4,7 @@ public class soysauce extends oneTimeUpgrade
 {
     public soysauce()
     {
-        super(false, 10, 1.5, "SoySauce");
+        super(false, 1000, 100, "SoySauce");
         MayflowerImage p = new MayflowerImage ("img/SoySauce.png");
         p.scale(80,80);
         setImage(p);
