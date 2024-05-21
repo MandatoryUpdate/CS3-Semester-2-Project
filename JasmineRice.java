58a9477419a2b9933860dba44f45211924c045c0
MandatoryUpdate <118033042+MandatoryUpdate@users.noreply.github.com>
2024-05-21 08:44:23 -0500

    Update JasmineRice.java

--- JasmineRice.java
+++ JasmineRice.java
@@ -3,6 +3,7 @@ public class JasmineRice extends Upgrades{
     private boolean bought;
     private int cost;
     public JasmineRice(){
+        //The order is as follows, buildings (amount of buildings), production (how much they make), cost, timer (how long it takes to gen), positionX, positionY
         super(0,20,100, 10000, 645, 379);
         MayflowerImage p1 = new MayflowerImage ("img/JasmineRice.png");
         p1.scale(115,115);
