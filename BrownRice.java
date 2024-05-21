c1db9652e9a10d242cbd82a641a47354bf4c6e5c
dhanushmitt <167135731+dhanushmitt@users.noreply.github.com>
2024-05-20 22:44:49 -0500

    Update BrownRice.java

--- BrownRice.java
+++ BrownRice.java
@@ -5,7 +5,7 @@ public class BrownRice extends Upgrades{
     public BrownRice(){
         super(0,75,500, 10000);
         MayflowerImage p1 = new MayflowerImage ("img/BrownRice.png");
-        p1.scale(150,100);
+        p1.scale(135,90);
         setImage(p1);
     }
     
