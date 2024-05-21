b95e481521a9542eeb0c5e1e7b87956fab15707f
brandonwchen2 <167358534+brandonwchen2@users.noreply.github.com>
2024-05-21 09:09:05 -0500

    Update tofu.java

--- tofu.java
+++ tofu.java
@@ -4,7 +4,7 @@ public class tofu extends oneTimeUpgrade
 {
     public tofu()
     {
-        super(false, 10, 1.5, "tofu");
+        super(false, 100, 10, "tofu");
         MayflowerImage p = new MayflowerImage ("img/tofu.png");
         p.scale(80,80);
         setImage(p);
