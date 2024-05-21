e777563d00cd7e7fa165884e2e99f8e4a98b2242
brandonwchen2 <167358534+brandonwchen2@users.noreply.github.com>
2024-05-21 09:14:32 -0500

    Update BiriyaniRice.java

--- BiriyaniRice.java
+++ BiriyaniRice.java
@@ -4,7 +4,7 @@ public class BiriyaniRice extends Upgrades{
     private int cost;
     public BiriyaniRice(){
         //The order is as follows, buildings (amount of buildings), production (how much they make), cost, timer (how long it takes to gen), positionX, positionY
-        super(0,50,5000, 10000, 645, 460);
+        super(0,50,5000, 10000, 5000, 460);
         MayflowerImage p1 = new MayflowerImage ("img/BiriyaniRice.png");
         p1.scale(115,115);
         setImage(p1);
