da319402366d2ae95db79242439989a6aceaa6c1
brandonwchen2 <167358534+brandonwchen2@users.noreply.github.com>
2024-05-21 09:21:34 -0500

    Update BasmatiRice.java

--- BasmatiRice.java
+++ BasmatiRice.java
@@ -4,7 +4,7 @@ public class BasmatiRice extends Upgrades{
     private int cost;
     public BasmatiRice(){
         //The order is as follows, buildings (amount of buildings), production (how much they make), cost, timer (how long it takes to gen), positionX, positionY
-        super(0,5000,50000, 10000, 5000, 547);
+        super(0,5000,50000, 10000, 645, 547);
         MayflowerImage p1 = new MayflowerImage ("img/BasmatiRice.png");
         p1.scale(100,100);
         setImage(p1);
