7838a1b5cd2de42c11e7623391183506333c2cda
MandatoryUpdate <118033042+MandatoryUpdate@users.noreply.github.com>
2024-05-21 08:42:14 -0500

    Update JasmineRice.java

--- JasmineRice.java
+++ JasmineRice.java
@@ -3,7 +3,7 @@ public class JasmineRice extends Upgrades{
     private boolean bought;
     private int cost;
     public JasmineRice(){
-        super(0,20,100, 10000);
+        super(0,20,100, 10000, 645, 379);
         MayflowerImage p1 = new MayflowerImage ("img/JasmineRice.png");
         p1.scale(115,115);
         setImage(p1);
