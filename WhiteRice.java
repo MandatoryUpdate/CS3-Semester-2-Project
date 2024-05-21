dccc212074467d9cbf9e7fcbd15033a39131a29e
brandonwchen2 <167358534+brandonwchen2@users.noreply.github.com>
2024-05-21 09:13:23 -0500

    Update WhiteRice.java

--- WhiteRice.java
+++ WhiteRice.java
@@ -4,7 +4,7 @@ public class WhiteRice extends Upgrades{
     private int cost;
     public WhiteRice(){
         //The order is as follows, buildings (amount of buildings), production (how much they make), cost, timer (how long it takes to gen), positionX, positionY
-        super(0,1,20,10, 645, 90);
+        super(0,1,20,10, 5000, 90);
         MayflowerImage p1 = new MayflowerImage ("img/WhiteRice.png");
         p1.scale(100,100);
         setImage(p1);
