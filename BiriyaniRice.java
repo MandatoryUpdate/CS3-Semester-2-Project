fbe5529ee309a93aaa60eb718adade9697ad9fba
MandatoryUpdate <118033042+MandatoryUpdate@users.noreply.github.com>
2024-05-21 08:45:50 -0500

    Update BiriyaniRice.java

--- BiriyaniRice.java
+++ BiriyaniRice.java
@@ -3,7 +3,8 @@ public class BiriyaniRice extends Upgrades{
     private boolean bought;
     private int cost;
     public BiriyaniRice(){
-        super(0,600,5000, 10000);
+        //The order is as follows, buildings (amount of buildings), production (how much they make), cost, timer (how long it takes to gen), positionX, positionY
+        super(0,600,5000, 10000, 645, 460);
         MayflowerImage p1 = new MayflowerImage ("img/BiriyaniRice.png");
         p1.scale(115,115);
         setImage(p1);
